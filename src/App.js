c1a0a2f28be5dc2b3200a3a916ccf608df38255f
Ar1kol <108225943+Ar1kol@users.noreply.github.com>
2022-12-18 10:57:18 +0200

    predeploy

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import { useState, useEffect } from "react";
 import { Outlet } from "react-router-dom";
 import Preloader from "../src/components/Pre";
 import Navbar from "./components/Navbar";
